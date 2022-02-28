12ff917d4da63806aa0ee8d89094bb985b069d8c
vichShir <victor.shirasuna@gmail.com>
2022-02-28 19:35:48 -0300

    Modificado scrpit de streaming history para adicionar coluna de podcast

--- scripts/data-wrangling/StreamingHistory.R
+++ scripts/data-wrangling/StreamingHistory.R
@@ -7,6 +7,7 @@ getwd()
 # import necessary libraries
 install.packages("rjson")
 library(rjson)
+library(dplyr)
 
 # import data
 streaming_file <- "../../data/raw/StreamingHistory0.json"
@@ -34,6 +35,24 @@ streaming_hist <- data.frame("endTime" = endTimes, "artistName" = artistNames,
 View(streaming_hist)
 str(streaming_hist)
 summary(streaming_hist)
+
+############################
+# Create is_podcast column #
+############################
+# all artists
+all_artists <- unique(streaming_hist$artistName)
+all_artists
+
+# filter podcast artists
+podcast_artists = all_artists[c(1, 21, 22)]
+podcast_artists
+
+# create column
+streaming_hist <- streaming_hist %>%
+  mutate(isPodcast = c("No", "Yes")[(streaming_hist$artistName %in% podcast_artists)+1])
+
+# verify all podcast tracks
+filter(streaming_hist, artistName %in% podcast_artists)
 
 # export data frame
 write.csv(streaming_hist,"../../data/csv/streaming_history.csv", row.names = FALSE)
