cbe4f7d28eb84d653ede4ceb46363744fc9ba888
vichShir <victor.shirasuna@gmail.com>
2022-03-01 15:45:30 -0300

    Melhorado extração das músicas

--- scripts/data-wrangling/StreamingHistory.R
+++ scripts/data-wrangling/StreamingHistory.R
@@ -38,6 +38,32 @@ View(streaming_hist)
 str(streaming_hist)
 summary(streaming_hist)
 
+#####################
+# Duplicated Tracks #
+#####################
+# see trackName structure
+streaming_hist[sample(nrow(streaming_hist), 10),]
+typeof(streaming_hist$trackName)
+
+# get only the music title
+?strsplit
+streaming_hist$trackName <- sapply(streaming_hist$trackName, function(x) trimws(unlist(strsplit(as.character(x), split = "-"))[1]))
+streaming_hist$trackName <- sapply(streaming_hist$trackName, function(x) trimws(unlist(strsplit(as.character(x), split = "(", fixed = TRUE))[1]))
+streaming_hist$trackName <- sapply(streaming_hist$trackName, function(x) trimws(unlist(strsplit(as.character(x), split = ":", fixed = TRUE))[1]))
+streaming_hist$trackName <- sapply(streaming_hist$trackName, function(x) trimws(unlist(strsplit(as.character(x), split = "/", fixed = TRUE))[1]))
+streaming_hist$trackName <- sapply(streaming_hist$trackName, function(x) trimws(gsub("\"", "", x)))
+streaming_hist$trackName <- as.factor(streaming_hist$trackName)
+
+# remove missing values
+# NAs -> (Don't Fear) The Reaper?
+streaming_hist <- streaming_hist[complete.cases(streaming_hist), ]
+
+View(streaming_hist)
+str(streaming_hist)
+summary(streaming_hist)
+
+streaming_hist[sample(nrow(streaming_hist), 100),]
+
 ############################
 # Create is_podcast column #
 ############################
@@ -55,6 +81,7 @@ streaming_hist <- streaming_hist %>%
 
 # verify all podcast tracks
 filter(streaming_hist, artistName %in% podcast_artists)
+View(streaming_hist)
 
 #########################
 # Create trackID column #
@@ -74,36 +101,53 @@ search_track_id <- function(artistName, trackName)
   return(result)
 }
 
-get_uri_from_search <- function(result_spotify, trackName)
+get_id_from_search <- function(result_spotify, trackName)
 {
+  # search attributes
   album_names <- result_spotify$album.name
   track_names <- result_spotify$name
   album_size <- length(album_names)
   
+  # display
   cat("\014")
   print(trackName)
   
-  # search is empty
+  # track not found
   if(is.null(album_names))
   {
     return("null")
   }
   
   # matchs first album track
-  if(trackName == track_names[1] || length(album_names) == 1)
+  album_index <- 0
+  if(track_names[1] == trackName || length(album_names) == 1)
   {
     album_index <- 1
   }
   else
   {
-    cat("Albuns:\n")
-    for(i in 1:album_size)
+    # verify each album
+    for(i in 2:album_size)
     {
-      print(paste("(", i, ")", album_names[i], "(", track_names[i], ")"))
+      if(track_names[i] == trackName)
+      {
+        album_index <- i
+        break
+      }
     }
     
-    cat("\nDigite um numero entre 1 e", album_size, "para selecionar o album da musica\n")
-    album_index <- as.numeric(readline(">> ")) 
+    # manual search
+    if(album_index == 0)
+    {
+      cat("Albuns:\n")
+      for(i in 1:album_size)
+      {
+        print(paste("(", i, ")", album_names[i], "(", track_names[i], ")"))
+      }
+      
+      cat("\nDigite um numero entre 1 e", album_size, "para selecionar o album da musica\n")
+      album_index <- as.numeric(readline(">> "))  
+    }
   }
   
   return(result_spotify$id[album_index])
@@ -111,8 +155,17 @@ get_uri_from_search <- function(result_spotify, trackName)
 
 # unique streaming tracks
 all_tracks <- streaming_hist[!duplicated(streaming_hist$trackName),]
+
+# remove unlike columns
+all_tracks$endTime <- NULL
+all_tracks$msPlayed <- NULL
+
+# sort by artist
+all_tracks <- all_tracks[order(all_tracks$artistName, all_tracks$trackName),]
+row.names(all_tracks) <- NULL
 nrow(all_tracks)
 View(all_tracks)
+str(all_tracks)
 
 # get ids
 ids <- c()
@@ -122,6 +175,7 @@ for (row in 1:nrow(all_tracks))
   artistName <- all_tracks[row, "artistName"]
   trackName <- all_tracks[row, "trackName"]
   
+  # skip podcasts
   if(isPodcast == "Yes")
   {
     ids <- c(ids, "null")
@@ -130,19 +184,23 @@ for (row in 1:nrow(all_tracks))
   
   cat(row, "de", nrow(all_tracks))
   src <- search_track_id(artistName, trackName)
-  trackID <- get_uri_from_search(src, trackName)
+  trackID <- get_id_from_search(src, trackName)
   print(trackID)
+  
+  # store trackID
   ids <- c(ids, trackID)
 }
-ids
+head(ids)
 length(ids)
 
-# append to dataframe
+# append ids to dataframe
 all_tracks$id <- ids
+all_tracks$id <- as.factor(all_tracks$id)
 View(all_tracks)
+summary(all_tracks)
 
 # export data frame
-write.csv(all_tracks,"../../data/csv/streaming_unique.csv", row.names = FALSE)
+write.csv(all_tracks,"../../data/csv/streaming/streaming_unique.csv", row.names = FALSE)
 
 # export data frame
-write.csv(streaming_hist,"../../data/csv/streaming_history.csv", row.names = FALSE)
+write.csv(streaming_hist,"../../data/csv/streaming/streaming_history.csv", row.names = FALSE)
